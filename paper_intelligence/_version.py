74265fb44375b86d0a93d290af52e83322530b10
Oded Falik <falik.oded.1@gmail.com>
2026-08-28 17:27:27 -0700

    Release paper-intelligence 0.5.1

--- paper_intelligence/_version.py
+++ paper_intelligence/_version.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import Optional
 
-__version__ = "0.2.0"
+__version__ = "0.5.1"
 
 # Version compatibility rules:
 # - Major version change = incompatible, must re-process
